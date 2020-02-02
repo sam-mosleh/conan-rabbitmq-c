470f229666499e1698561f32c83b846a4adc20e2
Sam <sam.mosleh@ut.ac.ir>
2020-02-02 21:18:42 +0330

    Removed MacOS from travis because of OpenSSL bug in brew. Added pagination.

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -9,7 +9,7 @@
 int main(int argc, char const *argv[]) {
     amqp_connection_state_t conn = amqp_new_connection();
     std::cout << std::endl
-              << "-------------------->Tests are done now.<----------------------"
+              << "----------------->Tests are done for now.<---------------------"
               << std::endl
               << "///////////////////////////////////////////////////////////////"
               << std::endl;
